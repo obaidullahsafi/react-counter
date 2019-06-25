c8010529d7aefd2c7acdee7a37649cb3c9529a0a
obaidullah <obaidullahsafi1990@gmail.com>
2019-06-25 16:02:40 +0200

    counter react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        count: 0,
+        backgroundColor: '',
+        name:'',
+
+
+    };
+
+
+    render() {
+        console.log(this.state.backgroundColor)
+        return (
+            <div className="App">
+                <h1 style={{backgroundColor:this.state.backgroundColor}}>Counter</h1>
+                <h1>{this.state.count}</h1>
+                <button onClick={this.incrementCounter}>+</button>
+                <button onClick={this.decrementCounter}>-</button>
+
+                <p>{this.state.name}</p>
+            </div>
+        );
+    }
+    incrementCounter =()=>{
+        console.log('incrementing')
+        this.setState({
+            count:this.state.count + 1,
+            backgroundColor:'green',
+            name:'obadullah'
+
+        })
+    };
+
+    decrementCounter =()=>{
+        console.log('decrementing')
+        this.setState({
+            count:this.state.count -1,
+            backgroundColor:'red',
+            name:'dzengiz'
+
+        })
+
+    }
 }
 
 export default App;

49b7a2147c0d2e318dc3ac0b40b0b571e8103fad
obaidullah <obaidullahsafi1990@gmail.com>
2019-07-01 17:19:44 +0200

    working met if en state

--- src/App.js
+++ src/App.js
@@ -1,35 +1,55 @@
 import React, {Component} from 'react';
 import './App.css';
-
 class App extends Component {
     state = {
         count: 0,
         backgroundColor: '',
         name:'',
-
-
+        width:'',
+        toggle:false,
+        buttonText:' naar links',
+         counterText: 'counterMidden',
+        clicked : false,
+        fontWeight:'normal'
     };
 
-
     render() {
+
+          const name = 'john'
+         const loading = true;
+        // if(loading){
+        //     return <h1>loading ....</h1>
+        // }
+
         console.log(this.state.backgroundColor)
         return (
+
+
             <div className="App">
-                <h1 style={{backgroundColor:this.state.backgroundColor}}>Counter</h1>
+                {loading ? <h1>loading ....</h1> : <h3>Hello {name}</h3>}
+                <h3 style={{width:this.state.width}}> Hello van react</h3>
+                {/*<h3>Hello {name}</h3>*/}
+                <h1 style={{backgroundColor:this.state.backgroundColor}}>{this.state.counterText}</h1>
                 <h1>{this.state.count}</h1>
                 <button onClick={this.incrementCounter}>+</button>
                 <button onClick={this.decrementCounter}>-</button>
+                <button onClick={this.size}>{this.state.buttonText}</button>
+                <button onClick={this.colorvr}> veranderd van de kleur</button>
+                <p style={{fontWeight:this.state.fontWeight}}>Hello David</p>
 
                 <p>{this.state.name}</p>
             </div>
+
         );
+
     }
     incrementCounter =()=>{
         console.log('incrementing')
         this.setState({
             count:this.state.count + 1,
             backgroundColor:'green',
-            name:'obadullah'
+            name:'obadullah',
+            counterText:'counterLinks'
 
         })
     };
@@ -39,10 +59,40 @@ class App extends Component {
         this.setState({
             count:this.state.count -1,
             backgroundColor:'red',
-            name:'dzengiz'
+            name:'dzengiz',
+            counterText:'countermidden'
 
         })
 
+    }
+    size =()=>{
+
+        // If toggled "on" (true)
+        if (this.state.toggle === false ){
+            this.setState({
+                width: 50,
+                toggle:true,
+                buttonText:'naar rechts'
+
+            })
+        } else {
+            this.setState({
+                width: '',
+                toggle:false,
+                buttonText:'links'
+
+            })
+        }
+    }
+    colorvr = ()=>{
+        console.log('click detected')
+        if(!this.state.clicked) {
+
+            this.setState({
+                fontWeight:'bold',
+                clicked: true
+            })
+        }
     }
 }
 
